d4ba693fb8cd8750db8970f16410863aa98eb392
Jackson Scott <scottja23@gmail.com>
2019-10-28 22:07:27 +0000

    shows chessboard

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,14 +1,14 @@
 module GamesHelper
-<<<<<<< HEAD
   def square_class(y_pos, x_pos)
     if y_pos % 2 == x_pos % 2
       return "white-square"
     else
       return "black-square"
     end
-=======
+  end
+
   def audio_path(source, options = {})
     path_to_asset(source, {type: :audio}.merge!(options))
->>>>>>> 91ca71d35155ff24e6b999aaceb5a76a61aa11d3
   end
+
 end
