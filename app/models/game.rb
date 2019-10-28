888a0efe58f2cc7da8b5f733856308acd9927eb9
Jackson Scott <scottja23@gmail.com>
2019-10-28 23:49:03 +0000

    board edits, images, models added

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,22 @@
 class Game < ApplicationRecord
+  has_many :pieces
+  has_many :users
 
+  after_create :initialize_pieces
+
+  def square_filled?(x_pos, y_pos)
+    if pieces.where("(x_pos = ? AND y_pos = ?)", x_pos, y_pos).any?
+      return true
+    else
+      return false
+    end
+  end
+
+  def initialize_pieces
+
+    (1..8).each do |x_pos|
+      Pawn.create(game_id: id, color: true, x_pos: x_pos, y_pos: 7, user_id: white_player_id, name: "white-pawn")
+    end
+  end
    
 end
