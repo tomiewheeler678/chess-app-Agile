cb917ace973942c3379cd040c526eb35be0319f8
tomiewheeler678 <tomiewheeler678@gmail.com>
2019-11-12 00:40:24 +0000

    errors 2

--- db/migrate/20191012154642_devise_create_users.rb
+++ db/migrate/20191012154642_devise_create_users.rb
@@ -3,11 +3,12 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :first_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :first_name, null: false, default: ""
-      t.string :last_name, null: false, default: ""
+
 
       ## Recoverable
       t.string   :reset_password_token
